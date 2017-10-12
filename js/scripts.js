5448c5ab6faf83216919161ec91ebf1c8a36277d
Bezrukov Mikhail <rdbmw@mail.ru>
2017-10-12 13:18:23 +0300

    backup commit

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(function() {
+
+  // // переключение слайдера
+  // var cases = document.getElementsByClassName("slider-cases__item");
+  //
+  // for (var i = 0; i < cases.length; i++) {
+  //   cases[i].addEventListener("click", function(event) {
+  //     event.preventDefault();
+  //     var slider = document.getElementById('slider');
+  //     var prev = document.getElementsByClassName('slider-cases__item--active')[0];
+  //     prev.classList.toggle("slider-cases__item--active");
+  //     this.classList.toggle("slider-cases__item--active");
+  //     slider.style.MozTransform = 'translate(calc(-100%/9*' + this.dataset.trans + '))';
+  //     slider.style.WebkitTransform = 'translate(calc(-100%/9*' + this.dataset.trans + '))';
+  //     slider.style.OTransform = 'translate(calc(-100%/9*' + this.dataset.trans + '))';
+  //     slider.style.MsTransform = 'translate(calc(-100%/9*' + this.dataset.trans + '))';
+  //     slider.style.transform = 'translate(calc(-100%/9*' + this.dataset.trans + '))';
+  //     //
+  //   });
+  // }
+});
